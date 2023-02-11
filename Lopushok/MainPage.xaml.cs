d70cd178d80315f3b6d7262e7d7cb1893bba46fa
GulnazTukhvatullina <gulnaz.tukhvatullina.02@inbox.ru>
2023-02-11 14:33:47 +0300

    Добавьте файлы проекта.

---
+++ Lopushok/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lopushok.DB;
+
+namespace Lopushok
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public List<DB.Product> Products { get; set; }
+        public List<DB.Product> FilterProduct { get; set; }
+        public List<DB.ProductType> Type { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            prod.ItemsSource = bd_connection.connection.Product.ToList();
+
+            
+        }
+
+        private void cb_productType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        public void Filter()
+        {
+            //FilterProduct = bd_connection.connection.Product.ToList();
+            //var searchingText = tb_search.Text.ToLower();
+            //var productType = cb_productType.SelectedItem as ProductType;
+
+            ////FilterProduct = Products.FindAll(product => product.Name.ToLower().Contains(searchingText));
+
+            //if (productType.Id != 0)
+            //    FilterProduct = Products.FindAll(product => product.ProductType == productType);
+        }
+
+        private void tb_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Filter();
+        }
+
+        private void prod_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void prod_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new ProductPage(prod.SelectedItem as DB.Product));
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ProductPage(new DB.Product()));
+        }
+    }
+}
---
+++ Lopushok/ProductPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lopushok.DB;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Lopushok
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public static DB.Product product { get; set; }
+        public List<ProductType> Types { get; set; }
+        public List<Workshop> Workshops { get; set; }
+        public ProductPage(DB.Product prod)
+        {
+            InitializeComponent();
+
+            product = prod;
+
+            Types = bd_connection.connection.ProductType.ToList();
+            Workshops = bd_connection.connection.Workshop.ToList();
+            this.DataContext = this;
+        }
+
+        private void btn_save_Click(object sender, RoutedEventArgs e)
+        {
+            if(product.Id == 0)
+                bd_connection.connection.Product.Add(product);
+            bd_connection.connection.SaveChanges();
+            NavigationService.Navigate(new MainPage());
+        }
+
+        private void btn_back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void btn_addImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog()
+            {
+                Filter = "*.jpg|*.jpg|*.png|*.png"
+            };
+            if (openFile.ShowDialog().GetValueOrDefault())
+            {
+                product.Image = File.ReadAllBytes(openFile.FileName);
+                img_product.Source = new BitmapImage(new Uri(openFile.FileName));
+            }
+        }
+    }
+}
